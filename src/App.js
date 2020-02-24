234a46d74db856e04de692cfe81d59087542d24e
phlong <phlong@vnpt.vn>
2020-02-24 16:44:39 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
+import TaskControl from './components/TaskControl';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks : [
+        {
+          id : 1,
+          name : 'abc',
+          status : false
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="text-center">
+          <h1>Quản Lý Công Việc</h1><hr />
+        </div>
+        <div className="row">
+          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+            <TaskForm />
+          </div>
+          <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+            <button type="button" className="btn btn-primary"  >
+              <span className="fa fa-plus mr-5"></span>
+              Thêm Công Việc
+            </button>
+
+            <button type="button" className="btn btn-danger" 
+            onClick ={() => {this.generataData()}}>
+              Thêm Data
+            </button>            
+                        
+            <TaskControl />
+            <TaskList />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
